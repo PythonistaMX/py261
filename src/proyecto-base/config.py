eba3a7e978ff69e13f46a270771d37158330e919
Jose Luis Chiquete <josech@gmail.com>
2022-08-04 11:06:16 -0500

    220804b

--- src/proyecto-base/config.py
+++ src/proyecto-base/config.py
@@ -1,5 +1,9 @@
 from os import environ
 
+if environ.get('ENVIRNMENT') == "PROD":
+    SQLALCHEMY_DATABASE_URL = 'oracle:///'
+
+
 if environ.get('GAE_ENV'):
     SQLALCHEMY_DATABASE_URL = 'sqlite:///'
 else:
